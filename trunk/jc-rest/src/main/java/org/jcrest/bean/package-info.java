9264fc25ccb85905d891c40a2849f8bde0633699
shane.k.j <shane.k.j@a0c51196-94a6-11dd-911f-3f62daa68977>
2008-11-12 23:31:55 +0000

    added the enunciate maven plugin to generate jax-rs/jaxb html documentation.

---
+++ trunk/jc-rest/src/main/java/org/jcrest/bean/package-info.java
@@ -0,0 +1,4 @@
+@XmlSchema (namespace = "http://www.jc-rest.org", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package org.jcrest.bean;
+
+import javax.xml.bind.annotation.XmlSchema;
---
+++ trunk/jc-rest/src/main/java/org/jcrest/bean/wrapper/package-info.java
@@ -0,0 +1,4 @@
+@XmlSchema (namespace = "http://www.jc-rest.org", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package org.jcrest.bean.wrapper;
+
+import javax.xml.bind.annotation.XmlSchema;
